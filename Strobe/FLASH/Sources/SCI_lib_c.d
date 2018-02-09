d77afda078434a233edcc66c5a9053f9e96fc2d2
Lucas M. Bacelo <bacelolucas@gmail.com>
2018-02-08 22:11:26 -0300

    Add files via upload

---
+++ Strobe/FLASH/Sources/SCI_lib_c.d
@@ -0,0 +1,10 @@
+
+Sources/SCI_lib_c.obj: \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/../Sources/SCI_lib.c \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/SCI_lib.h \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/derivative.h \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/mc9s08sh8.h \
+	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/hidef.h \
+	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/stddef.h \
+	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/stdtypes.h 
+
---
+++ Strobe/FLASH/Sources/main_c.d
@@ -0,0 +1,11 @@
+
+Sources/main_c.obj: \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/../Sources/main.c \
+	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/hidef.h \
+	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/stddef.h \
+	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/stdtypes.h \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/derivative.h \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/mc9s08sh8.h \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/LCD_lib.h \
+	C:/DATOS/CW\ Workspace/Strobe/Project_Headers/SCI_lib.h 
+
